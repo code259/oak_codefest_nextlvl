899ac19ecb757209f297b6c1844dda4cc9cb9bc2
bobby <speedcubersub50@gmail.com>
2021-01-23 11:13:08 +0530

    basic structure

--- app.js
+++ app.js
@@ -3,8 +3,16 @@
 // 1. git commit -m "type commit here"
 // 2. git push -u origin main
 
+const express = require('express');
+const path = require('path');
+const mogan = require('morgan');
 
+const app = express();
 
+app.set('view engine', 'ejs')
 
+app.use(express.static(path.join(__dirname, 'public')));
 
-console.log("Collaborative git test")
+app.listen(8080);
+
+app.use(morgan('dev'));
