6751857b9a519fae91d0ec41dbd04524bbcb0c7d
bobby <speedcubersub50@gmail.com>
2021-01-24 08:27:42 +0530

    timer done

--- public/js/timer.js
+++ public/js/timer.js
@@ -3,40 +3,38 @@ const timer = document.getElementById('stopwatch');
 var hr = 0;
 var min = 0;
 var sec = 0;
-var istop = true;
+var stoptime = true;
 
 function startTimer() {
-    if (istop == true) {
-        istop = false;
-        Cycle();
+    if (stoptime == true) {
+        stoptime = false;
+        timerCycle();
     }
 }
 
 function stopTimer() {
-    if (istop == false) {
-        istop = true;
+    if (stoptime == false) {
+        stoptime = true;
     }
 }
 
-function Cycle() {
-    console.log(sec)
-    if (istop == false) {
+function timerCycle() {
+    if (stoptime == false) {
         sec = parseInt(sec);
-        min = parseInt(sec);
-        hr = parseInt(sec);
+        min = parseInt(min);
+        hr = parseInt(hr);
 
-        sec = sec + 1
+        sec = sec + 1;
 
         if (sec == 60) {
             min = min + 1;
             sec = 0;
         }
         if (min == 60) {
-            hr = hr + 1
-            min = 0
+            hr = hr + 1;
+            min = 0;
+            sec = 0;
         }
-
-
         if (sec < 10 || sec == 0) {
             sec = '0' + sec;
         }
@@ -47,14 +45,14 @@ function Cycle() {
             hr = '0' + hr;
         }
 
-
         timer.innerHTML = hr + ':' + min + ':' + sec;
-        setTimeout("Cycle()", 1000)
+
+        setTimeout("timerCycle()", 1000);
     }
 }
 
 function resetTimer() {
-    timer.innerHTML = '00:00:00'
+    timer.innerHTML = '00:00:00';
     hr = 0;
     min = 0;
     sec = 0;
